7be5a59f2e7bb105655ce1d9fe03d48c52a92966
krishnaprasad k <prasadkrishna1189@gamil.com>
2024-05-07 19:33:24 +0530

    TS started

---
+++ trial.js
@@ -0,0 +1,5 @@
+//implicit
+var myString = "shabeeh";
+var myNumber = 123;
+// myString = 123               //Type 'number' is not assignable to type 'string'
+// myNumber = "shabeeh"        //Type 'string' is not assignable to type 'number'
---
+++ trial.ts
@@ -0,0 +1,11 @@
+//implicit
+
+let myString = "shabeeh";
+let myNumber = 123;
+
+// myString = 123               //Type 'number' is not assignable to type 'string'
+// myNumber = "shabeeh"        //Type 'string' is not assignable to type 'number'
+
+
+
+
